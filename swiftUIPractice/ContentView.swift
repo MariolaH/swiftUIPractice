efffe27a5de5e32d075e3bdfacd47596451045c1
Mariola <mariolahullings@gmail.com>
2024-01-27 16:25:29 -0500

    Created Reusable List

--- swiftUIPractice/ContentView.swift
+++ swiftUIPractice/ContentView.swift
@@ -9,16 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        List{
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
+            BookView(title: "Sum", author: "David Eagleman")
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
+            BookView(title: "Sum", author: "David Eagleman")
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
+            BookView(title: "Sum", author: "David Eagleman")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct BookView: View {
+    var title: String
+    var author: String
+    var body: some View {
+        HStack {
+            Image(systemName: "book")
+                .resizable()
+                .frame(width: 50, height: 40)
+                .foregroundColor(.pink)
+                .padding(10)
+            VStack(alignment: .leading, content: {
+                Text(title)
+                    .font(.title)
+                Text(author)
+                    .foregroundStyle(.gray)
+            } )
+            .padding(10)
+        }
+    }
 }
