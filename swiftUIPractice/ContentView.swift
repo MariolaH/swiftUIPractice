e855fdbd3f3179c4cbf3a28ec71e34ea647b46f4
Mariola <mariolahullings@gmail.com>
2024-01-27 15:31:26 -0500

    Initial Commit

---
+++ swiftUIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUIPractice
+//
+//  Created by Mariola Hullings on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUIPractice/swiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticeApp.swift
+//  swiftUIPractice
+//
+//  Created by Mariola Hullings on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
