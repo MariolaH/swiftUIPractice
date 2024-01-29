e4d453a951ff286ec84abfe39a3ae02bf105138f
Mariola <mariolahullings@gmail.com>
2024-01-28 22:17:13 -0500

    Made book color dynamic

--- swiftUIPractice/ContentView.swift
+++ swiftUIPractice/ContentView.swift
@@ -11,12 +11,12 @@ struct ContentView: View {
     var body: some View {
         
         List{
-            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
-            BookView(title: "Sum", author: "David Eagleman")
-            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
-            BookView(title: "Sum", author: "David Eagleman")
-            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken")
-            BookView(title: "Sum", author: "David Eagleman")
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken", bookColor: .pink)
+            BookView(title: "Sum", author: "David Eagleman", bookColor: .green)
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken", bookColor: .yellow)
+            BookView(title: "Sum", author: "David Eagleman", bookColor: .blue)
+            BookView(title: "A Tale of 2 Cites", author: "Charles Dicken", bookColor: .red)
+            BookView(title: "Sum", author: "David Eagleman", bookColor: .purple)
         }
     }
 }
@@ -28,12 +28,13 @@ struct ContentView: View {
 struct BookView: View {
     var title: String
     var author: String
+    var bookColor: Color
     var body: some View {
         HStack {
             Image(systemName: "book")
                 .resizable()
                 .frame(width: 50, height: 40)
-                .foregroundColor(.pink)
+                .foregroundColor(bookColor)
                 .padding(10)
             VStack(alignment: .leading, content: {
                 Text(title)
